9ef942cdb65f41ac827170c3b69b6a4fff5feaac
Musawirkorai <musawirkorai@gmail.com>
2025-05-18 10:45:28 +0500

    index file changed

---
+++ index.js
@@ -0,0 +1,4 @@
+var age = 25;
+var username = "Musawir Ali";
+var occupation = "Software Engineer";
+console.log("My name is ".concat(username, " and my age is ").concat(age, " and I am a ").concat(occupation));
