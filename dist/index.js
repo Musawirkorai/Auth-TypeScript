641a1221aa9ab10bc75d9210e5feb0640b3b6b03
Musawirkorai <musawirkorai@gmail.com>
2025-05-18 10:46:16 +0500

    index js file

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+let age = 25;
+let username = "Musawir Ali";
+let occupation = "Software Engineer";
+console.log(`My name is ${username} and my age is ${age} and I am a ${occupation}`);
+//# sourceMappingURL=index.js.map
