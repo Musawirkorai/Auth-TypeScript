7c12809a06fd973babe20c5b6f35eeb40eaf954b
Musawirkorai <musawirkorai@gmail.com>
2025-05-18 10:46:26 +0500

    ts file aded

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let age:number = 25;
+let username:string = "Musawir Ali";
+let occupation:string = "Software Engineer";
+
+console.log(`My name is ${username} and my age is ${age} and I am a ${occupation}`);
